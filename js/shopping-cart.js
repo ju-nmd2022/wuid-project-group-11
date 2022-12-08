56cf78516859d4b21b84b52643099f8c67fd2301
yousef <nayo21da@student.ju.se>
2022-12-08 10:13:56 +0100

    with js

---
+++ js/shopping-cart.js
@@ -0,0 +1,72 @@
+document.querySelector(".size-1").addEventListener("click", function () {
+  document.querySelector(".size-2").style.display = "none";
+  document.querySelector(".size-3").style.display = "none";
+  document.querySelector(".size-4").style.display = "none";
+  document.querySelector(".size-5").style.display = "none";
+  document.querySelector(".size-6").style.display = "none";
+  document.querySelector(".size-7").style.display = "none";
+  document.querySelector(".size-8").style.display = "none";
+});
+document.querySelector(".size-2").addEventListener("click", function () {
+  document.querySelector(".size-1").style.display = "none";
+  document.querySelector(".size-3").style.display = "none";
+  document.querySelector(".size-4").style.display = "none";
+  document.querySelector(".size-5").style.display = "none";
+  document.querySelector(".size-6").style.display = "none";
+  document.querySelector(".size-7").style.display = "none";
+  document.querySelector(".size-8").style.display = "none";
+});
+document.querySelector(".size-3").addEventListener("click", function () {
+  document.querySelector(".size-1").style.display = "none";
+  document.querySelector(".size-2").style.display = "none";
+  document.querySelector(".size-4").style.display = "none";
+  document.querySelector(".size-5").style.display = "none";
+  document.querySelector(".size-6").style.display = "none";
+  document.querySelector(".size-7").style.display = "none";
+  document.querySelector(".size-8").style.display = "none";
+});
+document.querySelector(".size-4").addEventListener("click", function () {
+  document.querySelector(".size-1").style.display = "none";
+  document.querySelector(".size-2").style.display = "none";
+  document.querySelector(".size-3").style.display = "none";
+  document.querySelector(".size-5").style.display = "none";
+  document.querySelector(".size-6").style.display = "none";
+  document.querySelector(".size-7").style.display = "none";
+  document.querySelector(".size-8").style.display = "none";
+});
+document.querySelector(".size-5").addEventListener("click", function () {
+  document.querySelector(".size-1").style.display = "none";
+  document.querySelector(".size-2").style.display = "none";
+  document.querySelector(".size-3").style.display = "none";
+  document.querySelector(".size-4").style.display = "none";
+  document.querySelector(".size-6").style.display = "none";
+  document.querySelector(".size-7").style.display = "none";
+  document.querySelector(".size-8").style.display = "none";
+});
+document.querySelector(".size-6").addEventListener("click", function () {
+  document.querySelector(".size-1").style.display = "none";
+  document.querySelector(".size-2").style.display = "none";
+  document.querySelector(".size-3").style.display = "none";
+  document.querySelector(".size-4").style.display = "none";
+  document.querySelector(".size-5").style.display = "none";
+  document.querySelector(".size-7").style.display = "none";
+  document.querySelector(".size-8").style.display = "none";
+});
+document.querySelector(".size-7").addEventListener("click", function () {
+  document.querySelector(".size-1").style.display = "none";
+  document.querySelector(".size-2").style.display = "none";
+  document.querySelector(".size-3").style.display = "none";
+  document.querySelector(".size-4").style.display = "none";
+  document.querySelector(".size-5").style.display = "none";
+  document.querySelector(".size-6").style.display = "none";
+  document.querySelector(".size-8").style.display = "none";
+});
+document.querySelector(".size-8").addEventListener("click", function () {
+  document.querySelector(".size-1").style.display = "none";
+  document.querySelector(".size-2").style.display = "none";
+  document.querySelector(".size-3").style.display = "none";
+  document.querySelector(".size-4").style.display = "none";
+  document.querySelector(".size-5").style.display = "none";
+  document.querySelector(".size-6").style.display = "none";
+  document.querySelector(".size-7").style.display = "none";
+});
