d72d51ada6e804971c9734427e7e33fbf1f05212
yousef <nayo21da@student.ju.se>
2023-08-03 20:41:20 +0200

    gv

--- js/detail.js
+++ js/detail.js
@@ -1,17 +1,14 @@
 const shoppingCart = document.getElementById("extraShoppingCart");
-const close = document.getElementById("close");
+const addToShoppingCartButton = document.getElementById("button");
+const closeButton = document.getElementById("close");
 
 shoppingCart.style.display = "none";
 
-<<<<<<< HEAD
 addToShoppingCartButton.onclick = function () {
   localStorage.basketfull = true;
-=======
-button.addEventListener("click", function () {
->>>>>>> parent of 463c193 (Touch up)
   shoppingCart.style.display = "flex";
-});
+};
 
-close.onclick = function () {
+closeButton.onclick = function () {
   shoppingCart.style.display = "none";
 };
